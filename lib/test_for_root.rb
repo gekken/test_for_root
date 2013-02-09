252fec85b2c718bf6bfc826bc431d517ed113cf8
gekken <pbomars@gmail.com>
2013-02-09 14:07:06 -0800

    added several basic methods. need to figure out windows stuff

--- lib/test_for_root.rb
+++ lib/test_for_root.rb
@@ -1,5 +1,32 @@
 require "test_for_root/version"
 
 module TestForRoot
-  # Your code goes here...
+
+
+  class User
+
+    attr_reader :user, :member_of, :user_id
+
+    def initialize
+      @user = `id -un`.chomp
+      @member_of = (`groups #{@user}`.chomp.split(":"))[1].split
+      @user_id = %x{id -u}.chomp.to_i
+    end
+
+    def name
+      @user
+    end
+
+    def groups
+      @member_of
+    end
+
+    def member? (group_name)
+      @member_of.include? group_name
+    end
+
+    def uid
+      @user_id
+    end
+  end
 end
